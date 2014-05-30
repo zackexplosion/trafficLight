34b6c167ce286f9a375d0066eec0a87817570524
bency <bency80097@gmail.com>
2014-05-31 02:26:05 +0800

    refractor

--- index.php
+++ index.php
@@ -9,47 +9,55 @@
             function deg(deg){
                 return (Math.PI/180)*deg - (Math.PI/180)*90;
             }
-            function CountDown(){
-                var second = $('#canvas_seconds').get(0);
-                var timer = new Array();
-                var flag = 0;
-                timer[1] = 26;
-                timer[0] = 74;
+
+            /*
+            * trafficLight = {
+            *                   timer: [24, 76],
+            *                   period: int,
+            *                   offset: int,
+            *                   tl_start: int}
+             */
+            var countDown = function (trafficLight, display) {
+                var phase = (Math.round((new Date()).getTime() / 1000 ) - trafficLight.tl_start + trafficLight.offset) % trafficLight.period;
+                var flag = null;
+                var counter = null;
+                if(phase > trafficLight.timer[0]){
+                    counter = trafficLight.period - phase;
+                    flag = 1;
+                }
+                else{
+                    flag = 0;
+                    counter = trafficLight.timer[0] - phase;
+                }
+                $(".clock_seconds .val").text(Math.floor(counter));
+                var ctx = display.getContext('2d');
+                ctx.clearRect(0, 0, display.width, display.height);
+                ctx.beginPath();
+                if(flag == 0)
+                    ctx.strokeStyle = "#ff6565";
+                else if(flag == 1)
+                    ctx.strokeStyle = "#9cdb7d";
+                ctx.shadowBlur = 10;
+                ctx.shadowOffsetX = 0;
+                ctx.shadowOffsetY = 0;
+                ctx.shadowColor = "none";
+
+                ctx.arc(94,94,85, deg(0), deg(Math.floor(360 - counter * 360 / trafficLight.timer[flag])));
+                ctx.lineWidth = 17;
+                ctx.stroke();
+            }
+            $(document).ready(function(){
                 var today = new Date();
                 var tl_start = Math.round((new Date(today.getFullYear(),today.getMonth(),today.getDate() - 1,7)).getTime()/1000);
-                var period = timer[0] + timer[1];
-                var counter = 0;
-                var phase = 0;
-                var offset = 7;
-                setInterval(function(){
-                    phase = (Math.round((new Date()).getTime() / 1000 ) - tl_start + offset) % period;
-                    if(phase > timer[0]){
-                        counter = period - phase;
-                        flag = 1;
-                    }
-                    else{
-                        flag = 0;
-                        counter = timer[0] - phase;
-                    }
-                    $(".clock_seconds .val").text(Math.floor(counter));
-                    var ctx = second.getContext('2d');
-                    ctx.clearRect(0, 0, second.width, second.height);
-                    ctx.beginPath();
-                    if(flag == 0)
-                        ctx.strokeStyle = "#ff6565";
-                    else if(flag == 1)
-                        ctx.strokeStyle = "#9cdb7d";
-                    ctx.shadowBlur = 10;
-                    ctx.shadowOffsetX = 0;
-                    ctx.shadowOffsetY = 0;
-                    ctx.shadowColor = "none";
-
-                    ctx.arc(94,94,85, deg(0), deg(Math.floor(360 - counter * 360 / timer[flag])));
-                    ctx.lineWidth = 17;
-                    ctx.stroke();
-                },1000);
-            }
-        $(document).ready(function(){CountDown();});
+                var trafficLight = {
+                    timer: [76, 24],
+                    period: 100,
+                    offset: 0,
+                    tl_start: tl_start
+                };
+                var second = $('#canvas_seconds').get(0);
+                var startCount = setInterval(function(){countDown(trafficLight, second)}, 1000);
+            });
         </script>
     </head>
     <body>
