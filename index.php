94ae2f59895ddd82e7f8973d7ad775a0904afb5e
bency <bency80097@gmail.com>
2016-10-19 22:25:38 +0800

    新增 getParameterByName

--- index.php
+++ index.php
@@ -111,6 +111,14 @@
             </div>
         </div>
         <script type="text/javascript">
+            var getParameterByName = function (name) {
+                var url = window.location.href;
+                name = name.replace(/[\[\]]/g, "\\$&");
+                var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
+                if (!results) return null;
+                if (!results[2]) return '';
+                return (decodeURIComponent(results[2].replace(/\+/g, " ")));
+            }
             var lightInterval = null;
             var redMeasure = null;
             var greenMeasure = null;
